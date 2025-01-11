5d112f6ccd1402b91f6c825e302af5ed0d393d55
Sakib Fakir <fakirsakib22232>
2025-01-12 00:24:10 +0600

    done payment

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-
+const stripe = require('stripe')(process.env.Stripe_secrect_key);
 
 app.use(cors());
 app.use(express.json())
@@ -34,7 +35,11 @@ async function run() {
 
     // Database 
 
-    const productsCollection = client.db('product').collection('products')
+    const productsCollection = client.db('product').collection('products');
+    const cartCollection = client.db('cart').collection("carts");
+    const paymentsCollection = client.db('cart').collection("payments");
+
+
 
    
 
@@ -60,16 +65,61 @@ async function run() {
 
       const item = req.body;
 
-      const result = await productsCollection.insertOne(item);
+      const result = await cartCollection.insertOne(item);
       res.send(result);
 
 
 
     })
-    // get 
+    // get cart 
+
+    app.get('/carts', async (req,res)=>{
+      const result = await cartCollection.find().toArray();
+      res.send(result);
+    })
 
 
     // implement stripe
+
+    // create payment initent 
+
+    app.post('/create-payment-intent' , async (req,res)=>{
+
+      const {price} = req.body;
+      const amount = parseInt(price*100);
+      console.log(amount,'inside ammount')
+
+      const paymentIntent= await stripe.paymentIntents.create({
+        amount : amount,
+        currency : 'usd',
+        payment_method_types:['card']
+
+
+
+      })
+      res.send({
+        clientSecret: paymentIntent.client_secret 
+      })
+
+    })
+
+
+    // payment post data 
+
+    app.post('/payments' , async (req,res)=>{
+      const payment = req.body;
+      const paymentResult = await paymentsCollection.insertOne(payment);
+   
+
+      // delete payment id 
+      const query = {_id : {
+        // $in : paymentCards.map(id => new object(id))
+
+     
+      }}
+      // delete many (id);
+      res.send(paymentResult);
+    })
 
 
     
